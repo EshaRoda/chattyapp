3cc2c38382c0b8b340615bf11e4419b93fdb83cf
Esha Roda <esha.roda@gmail.com>
2017-08-22 18:56:05 +0000

    updated content of jsx files

---
+++ src/MessageList.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from ‘react’;
+import MessageList from ‘./MessageList.jsx’
+import ChatBar from ‘./ChatBar.jsx’
+import NavBar from ‘./NavBar.jsx’
+
+class App extends Component {
+ render() {
+   return (
+       <div>
+           <NavBar/>
+           <MessageList/>
+           <ChatBar/>
+       </div>
+
+ );
+ }
+}
+export default App;
